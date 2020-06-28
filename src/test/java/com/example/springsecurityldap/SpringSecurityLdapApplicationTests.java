2b40b3735fd76bd198026fca532183b0dfbce0d7
Luk Pheakdey <luk.pheakdey@gmail.com>
2020-06-28 17:34:15 -0400

    spring security with LDAP

---
+++ src/test/java/com/example/springsecurityldap/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
